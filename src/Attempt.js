fe37b4d194ea3828412df179a8bbe905339ede20
Kalindu <59674165+kalindu-k@users.noreply.github.com>
2020-03-24 14:10:10 -0400

    Add react spring attempt animation

--- src/Attempt.js
+++ src/Attempt.js
@@ -1,12 +1,17 @@
 import React from "react"
+import {Spring} from "react-spring/renderprops"
 import "./Attempt.css"
 class Attempt extends React.Component{
     render(){ //attempt component which holds an attempt at decrypting text with a given shift
         return(
-            <div style ={{backgroundColor:this.props.color}} className="attempt">
-                <h4>Decode shift: {this.props.value}</h4>
-                <p>{this.props.decoded}</p>
-            </div>
+            <Spring from={{opacity:0}} to={{opacity:1}}>
+                {props=>(  //react spring library for animations
+                    <div style={{...props,...{ backgroundColor: this.props.color }}} className="attempt">
+                        <h4>Decode shift: {this.props.value}</h4>
+                        <p>{this.props.decoded}</p>
+                    </div>
+                )}
+            </Spring>
         )
     }
 }
